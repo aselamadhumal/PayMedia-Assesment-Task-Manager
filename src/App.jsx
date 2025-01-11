79364ef00d242862b8a67925328c44328468ef96
Asela Madhumal Kumara <madhumalkumara2021@gmail.com>
2025-01-11 14:55:38 -0800

    change app.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TaskBoard from "./components/TaskBoard";
+import "./styles/index.css";
+
+const App = () => {
+  return (
+    <div>
+      <TaskBoard />
+    </div>
+  );
+};
+
+export default App;
