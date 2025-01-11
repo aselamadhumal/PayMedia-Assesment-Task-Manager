a1314de15dbc37c02e5e1fa77733f468e5aabb30
Asela Madhumal Kumara <madhumalkumara2021@gmail.com>
2025-01-11 14:50:22 -0800

    create api servise file

---
+++ src/api/tasks.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/tasks";
+
+export const getTasks = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+export const addTask = async (task) => {
+  const response = await axios.post(API_URL, task);
+  return response.data;
+};
+
+export const updateTask = async (id, updates) => {
+  const response = await axios.put(`${API_URL}/${id}`, updates);
+  return response.data;
+};
